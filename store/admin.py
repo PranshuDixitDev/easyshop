7859e7f5bfb1fd25dfd274713ed3118200877ad4
PranshuDixitDev <pranshudixit.dev@yahoo.com>
2024-02-15 23:33:47 +0530

    Adding and grouping item variations.
    Grouping product, cart increment , decrement , remove variations of the product.

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Variation
 # Register your models here.
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock',
-                    'category', 'modefied_date')
+                    'category', 'modefied_date', 'is_available')
     prepopulated_fields = {'slug':('product_name',)} 
     
 
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value',
+                    'is_active')
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value',
+                    'is_active') #here we are showing a box of filters using this line.
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)
