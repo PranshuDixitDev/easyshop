00856e96560664e49a0a7ca556f8ceea59e4e6ee
PranshuDixitDev <pranshudixit.dev@yahoo.com>
2024-02-16 14:36:41 +0530

    Registration form error
    I am able to see form but when submit i am not able to save the data and see it in django admin panel .

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,8 +3,26 @@ from .models import Account
 
 
 class RegistrationForm(forms.ModelForm):
-    
+
+    passwords = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter password',
+        'class': 'form-control',
+    }))
+
+    confirm_passwords = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm password'
+    }))
+
     class Meta:
         model = Account
-        fields = ['first_name', 'last_name', 'phone_number', 
+        fields = ['first_name', 'last_name', 'phone_number',
                   'email', 'password']
+        
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, *kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email address'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from .forms import RegistrationForm
+from .models import Account
 
 
 # Create your views here.
 def register(request):
-    form = RegistrationForm()
+    if request =='POST':
+        form = RegistrationForm(request.POST)
+        print(f"&&&&&&&&&&&, {form.is_valid()}")
+        print(f"Cleaned data: {form.cleaned_data}")
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            phone_number = form.cleaned_data['phone_number']
+            user = Account.objects.create_user(
+                first_name="first_name", last_name="last_name",
+                email="test@example.com", username="username", password="password")
+            user.phone_number = phone_number
+            user.save()
+    else:
+        form = RegistrationForm()
     context = {
         'form' : form, 
     }
