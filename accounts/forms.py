e395b9642727dc9ac358bce9833f17be7eb7ee62
PranshuDixitDev <pranshudixit.dev@yahoo.com>
2024-02-16 22:45:22 +0530

    Registration form working, Messages are Next

--- accounts/forms.py
+++ accounts/forms.py
@@ -17,7 +17,8 @@ class RegistrationForm(forms.ModelForm):
         model = Account
         fields = ['first_name', 'last_name', 'phone_number',
                   'email', 'password']
-        
+
+
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
         self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
@@ -26,3 +27,20 @@ class RegistrationForm(forms.ModelForm):
         self.fields['email'].widget.attrs['placeholder'] = 'Enter Email address'
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+            
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password and confirm_password:  # Check if both passwords are present
+            if password != confirm_password:
+                raise forms.ValidationError(
+                    "Password does not match."
+                )
+        else:
+            raise forms.ValidationError(
+                "Please enter both password and confirm password fields."
+            )
+
+
